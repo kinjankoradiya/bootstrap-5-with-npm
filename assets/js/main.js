44ed1da121731f24edfe35d0014bfda063d523eb
Kinjan <53822503+kinjankoradiya@users.noreply.github.com>
2023-02-15 17:06:05 +0530

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// JS Which needs to add
+document.write(
+  '<script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>' +
+    '<script src="assets/js/custom.js"></script>'
+);
